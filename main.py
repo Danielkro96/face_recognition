3e55c5b49c0246ed742234709edfee68ed0cd30a
Daniel Kro <danielkro96@gmail.com>
2024-05-06 17:37:20 +0300

    add face compare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import face_recognition, os
+import face_recognition
+import os
 import cv2 as cv
 
 
@@ -13,19 +14,42 @@ def encode_faces(dir_path: str):
     return list_people_encoded
 
 
-def detect_faces(frame):
+def detect_faces(frame, known_encodings, known_names):
     frame_rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
     frame_small = cv.resize(frame_rgb, (0, 0), fx=0.5, fy=0.5)
     face_locations = face_recognition.face_locations(frame_small)
     face_locations = [(top * 2, right * 2, bottom * 2, left * 2) for (top, right, bottom, left) in face_locations]
 
     for top, right, bottom, left in face_locations:
+        face_encoding = face_recognition.face_encodings(frame_rgb, [(top, right, bottom, left)])[0]
+
+        # Compare face encoding with known encodings
+        matches = face_recognition.compare_faces(known_encodings, face_encoding)
+        name = "Unknown"
+
+        if True in matches:
+            first_match_index = matches.index(True)
+            name = known_names[first_match_index]
+
         cv.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 4)
-        cv.putText(frame, "name", (left + 10, bottom + 30), cv.FONT_HERSHEY_SIMPLEX, 1.2, (200, 200, 200), 3)
+        cv.putText(
+            frame, name,
+            (left + 10, bottom + 30),
+            cv.FONT_HERSHEY_SIMPLEX, 1.2, (200, 200, 200), 3)
     cv.imshow("Faces found", frame)
 
 
 def detect_face_by_live_video():
+    known_encodings = []
+    known_names = []
+
+    # Encoding known faces
+    for face_file in os.listdir('known_faces'):
+        image = face_recognition.load_image_file(f'known_faces/{face_file}')
+        encoding = face_recognition.face_encodings(image)[0]
+        known_encodings.append(encoding)
+        known_names.append(face_file.strip(".jpg"))
+
     capture = cv.VideoCapture(0)
 
     if not capture.isOpened():
@@ -39,14 +63,11 @@ def detect_face_by_live_video():
             raise RuntimeError("Unable to capture frame")
 
         if skip_frames > 0:
-            cv.imshow("Faces found", frame)
+            # cv.imshow("Faces found", frame)
             skip_frames -= 1
             continue
 
-        # frame_rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-        detect_faces(frame)
-        # face_thread = threading.Thread(target=detect_faces, args=frame_rgb)
-        # face_thread.start()
+        detect_faces(frame, known_encodings, known_names)
 
         key = cv.waitKey(1)
         if key == ord('q'):
@@ -59,7 +80,4 @@ def detect_face_by_live_video():
 
 
 if __name__ == "__main__":
-    # detect_face("known_faces/Daniel.jpg")
-    encode_faces('known_faces')
     detect_face_by_live_video()
-
